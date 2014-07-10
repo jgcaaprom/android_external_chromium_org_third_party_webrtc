477e6bce6816b8bb433c2e6c44b377bdc766a3b3
Android Chromium Automerger <chromium-automerger@android>
2014-07-10 18:45:25 +0000

    Update makefiles after merge of Chromium at 282385
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I85d3e5fb3d9291809471c199df114e462a4739d6

--- modules/neteq.target.darwin-arm64.mk
+++ modules/neteq.target.darwin-arm64.mk
@@ -93,7 +93,6 @@ MY_CFLAGS_Debug := \
 	-Wno-unused-but-set-variable \
 	-Os \
 	-g \
-	-fomit-frame-pointer \
 	-fdata-sections \
 	-ffunction-sections \
 	-funwind-tables
@@ -221,7 +220,6 @@ MY_CFLAGS_Release := \
 	-fno-ident \
 	-fdata-sections \
 	-ffunction-sections \
-	-fomit-frame-pointer \
 	-funwind-tables
 
 MY_DEFS_Release := \
--- modules/rtp_rtcp.target.linux-arm64.mk
+++ modules/rtp_rtcp.target.linux-arm64.mk
@@ -90,7 +90,6 @@ MY_CFLAGS_Debug := \
 	-Wno-unused-but-set-variable \
 	-Os \
 	-g \
-	-fomit-frame-pointer \
 	-fdata-sections \
 	-ffunction-sections \
 	-funwind-tables
@@ -203,7 +202,6 @@ MY_CFLAGS_Release := \
 	-fno-ident \
 	-fdata-sections \
 	-ffunction-sections \
-	-fomit-frame-pointer \
 	-funwind-tables
 
 MY_DEFS_Release := \
