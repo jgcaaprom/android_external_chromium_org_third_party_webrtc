e508702084d9913b6b5f2d98a132580b29367bc1
Android Chromium Automerger <chromium-automerger@android>
2014-09-13 10:09:51 +0000

    Update makefiles after merge of Chromium at 6a4d455b8650
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I76592b034e4c868de1bd5ebafa48efc39ee50f0c

--- modules/audio_processing/gen_aecm_core_neon_offsets_h.target.linux-arm.mk
+++ modules/audio_processing/gen_aecm_core_neon_offsets_h.target.linux-arm.mk
@@ -6,7 +6,6 @@ LOCAL_MODULE_CLASS := GYP
 LOCAL_MODULE := third_party_webrtc_modules_audio_processing_gen_aecm_core_neon_offsets_h_gyp
 LOCAL_MODULE_STEM := gen_aecm_core_neon_offsets_h
 LOCAL_MODULE_SUFFIX := .stamp
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TARGET_ARCH := $(TARGET_$(GYP_VAR_PREFIX)ARCH)
 gyp_intermediate_dir := $(call local-intermediates-dir,,$(GYP_VAR_PREFIX))
 gyp_shared_intermediate_dir := $(call intermediates-dir-for,GYP,shared,,,$(GYP_VAR_PREFIX))
